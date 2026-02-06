0426e4afa68cc70b023fd913a0ad82b7e8f4c75e
Mateus <mhenrique4060@gmail.com>
2026-02-06 18:44:08 -0300

    feat: create archive and lambda resources
    Seria uma boa no futuro alterar o lambda para receber uma imagem do ECR, e não um zip.

--- infra/main.tf
+++ infra/main.tf
@@ -4,4 +4,27 @@ resource "aws_s3_bucket" "bucket" {
   tags = local.tags
 }
 
+data "archive_file" "code_package" {
+  type        = "zip"
+  source_file = "../src/lambda_function.py"
+  output_path = "../src/lambda_function.zip"
+}
 
+resource "aws_lambda_function" "lambda_function" {
+  filename         = data.archive_file.code_package.output_path
+  function_name    = "${local.name}-function"
+  role             = aws_iam_role.iam_for_lambda.arn
+  handler          = "lambda_function.lambda_handler"
+  source_code_hash = data.archive_file.code_package.output_path
+
+  runtime = "python3.12"
+
+  environment {
+    variables = {
+      env       = "dev"
+      log_level = "info"
+    }
+  }
+
+  tags = local.tags
+}
