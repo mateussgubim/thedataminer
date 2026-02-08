d591bfe4bdf06783ef381507df385e4660f88484
Mateus <mhenrique4060@gmail.com>
2026-02-07 21:55:11 -0300

    feat: create request to openmeteo

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,8 +1,15 @@
 import json
+import requests
+
+r = requests.post("https://api.open-meteo.com/v1/forecast", data={"latitude": 52.52,"longitude": 13.41,"hourly": "temperature_2m"})
+
 
 def lambda_handler(event, context):
+    r = requests.post("https://api.open-meteo.com/v1/forecast", data={"latitude": 52.52,"longitude": 13.41,"hourly": "temperature_2m"})
+
     print("Starting data miner...")
+    r.json()
     return {
         "statusCode": 200,
         "body": json.dumps("Hello, World!")
-    }
+}
